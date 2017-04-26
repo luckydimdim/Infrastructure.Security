e256356e15418cfdf77d735269b8a1fdfa444b96
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-04-26 13:56:53 +0300

    CMAS-375 Реализовать аутентификацию на backend'e

---
+++ Infrastructure.Security/ModuleSecurity.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using Nancy.Extensions;
+using System;
+using System.Security.Claims;
+
+namespace Cmas.Infrastructure.Security
+{
+    public static class ModuleSecurity
+    {
+        /// <summary>This module requires authentication</summary>
+        /// <param name="module">Module to enable</param>
+        public static void RequiresAuthentication(this INancyModule module)
+        {
+            module.AddBeforeHookOrExecute(SecurityHooks.RequiresAuthentication, "Requires Authentication");
+        }
+
+        /// <summary>
+        /// This module requires authentication and certain claims to be present.
+        /// </summary>
+        /// <param name="module">Module to enable</param>
+        /// <param name="requiredClaims">Claim(s) required</param>
+        public static void RequiresClaims(this INancyModule module, params Predicate<Claim>[] requiredClaims)
+        {
+            module.AddBeforeHookOrExecute(SecurityHooks.RequiresAuthentication, "Requires Authentication");
+            module.AddBeforeHookOrExecute(ctx => SecurityHooks.RequiresClaims(ctx, requiredClaims), "Requires Claims");
+        }
+
+    }
+}
---
+++ Infrastructure.Security/SecurityHooks.cs
@@ -0,0 +1,27 @@
+﻿using Nancy;
+using System; 
+using Cmas.Infrastructure.ErrorHandler;
+using Nancy.Security;
+using System.Security.Claims;
+
+namespace Cmas.Infrastructure.Security
+{
+    public static class SecurityHooks
+    {
+        public static Response RequiresAuthentication(NancyContext ctx)
+        {
+            if (!ctx.CurrentUser.IsAuthenticated())
+                throw new UnauthorizedErrorException();
+            else
+                return null;
+        }
+
+        public static Response RequiresClaims(NancyContext ctx, params Predicate<Claim>[] claims)
+        {
+            if (!ctx.CurrentUser.HasClaims(claims))
+                throw new ForbiddenErrorException();
+            else
+                return null;
+        }
+    }
+}
