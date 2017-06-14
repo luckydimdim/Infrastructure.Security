deec8b70ae44bb42ba7d685b4e6482c34c7cbefe
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-06-14 16:13:34 +0300

    file downloading

--- Infrastructure.Security/ModuleSecurity.cs
+++ Infrastructure.Security/ModuleSecurity.cs
@@ -1,6 +1,7 @@
 ﻿using Nancy;
 using Nancy.Extensions;
 using System;
+using System.Linq;
 using System.Security.Claims;
 
 namespace Cmas.Infrastructure.Security
@@ -9,9 +10,15 @@ namespace Cmas.Infrastructure.Security
     {
         /// <summary>This module requires authentication</summary>
         /// <param name="module">Module to enable</param>
-        public static void RequiresAuthentication(this INancyModule module)
+        public static void RequiresAuthentication(this INancyModule module, string[] except = null)
         {
-            module.AddBeforeHookOrExecute(SecurityHooks.RequiresAuthentication, "Requires Authentication");
+            module.AddBeforeHookOrExecute((ctx =>
+            {
+                if (except != null && except.Contains(ctx.ResolvedRoute.Description.Name))
+                    return null;
+
+                return SecurityHooks.RequiresAuthentication(ctx);
+            }), "Requires Authentication");
         }
 
         /// <summary>
@@ -19,17 +26,43 @@ namespace Cmas.Infrastructure.Security
         /// </summary>
         /// <param name="module">Module to enable</param>
         /// <param name="requiredClaims">Claim(s) required</param>
-        public static void RequiresClaims(this INancyModule module, params Predicate<Claim>[] requiredClaims)
+        public static void RequiresClaims(this INancyModule module, string[] except = null,
+            params Predicate<Claim>[] requiredClaims)
         {
-            module.AddBeforeHookOrExecute(SecurityHooks.RequiresAuthentication, "Requires Authentication");
-            module.AddBeforeHookOrExecute(ctx => SecurityHooks.RequiresClaims(ctx, requiredClaims), "Requires Claims");
+            module.AddBeforeHookOrExecute((ctx =>
+            {
+                if (except != null && except.Contains(ctx.ResolvedRoute.Description.Name))
+                    return null;
+
+                return SecurityHooks.RequiresAuthentication(ctx);
+            }), "Requires Authentication");
+
+            module.AddBeforeHookOrExecute((ctx =>
+            {
+                if (except != null && except.Contains(ctx.ResolvedRoute.Description.Name))
+                    return null;
+
+                return SecurityHooks.RequiresClaims(ctx, requiredClaims);
+            }), "Requires Claims");
         }
 
-        public static void RequiresAnyRole(this INancyModule module, Role[] requiredRoles)
+        public static void RequiresAnyRole(this INancyModule module, Role[] requiredRoles, string[] except = null)
         {
-            module.AddBeforeHookOrExecute(SecurityHooks.RequiresAuthentication, "Requires Authentication");
-            module.AddBeforeHookOrExecute(ctx => SecurityHooks.RequiresAnyRole(ctx, requiredRoles), "Requires Claims");
-        }
+            module.AddBeforeHookOrExecute((ctx =>
+            {
+                if (except != null && except.Contains(ctx.ResolvedRoute.Description.Name))
+                    return null;
 
+                return SecurityHooks.RequiresAuthentication(ctx);
+            }), "Requires Authentication");
+
+            module.AddBeforeHookOrExecute((ctx =>
+            {
+                if (except != null && except.Contains(ctx.ResolvedRoute.Description.Name))
+                    return null;
+
+                return SecurityHooks.RequiresAnyRole(ctx, requiredRoles);
+            }), "Requires Claims");
+        }
     }
 }
